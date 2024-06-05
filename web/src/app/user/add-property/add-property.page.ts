a3c2d29f9da26f463e2cde43953fcd0d6e3d7db7
foleyKant <krayediego@gmail.com>
2024-06-05 14:47:34 +0200

    ok

--- web/src/app/user/add-property/add-property.page.ts
+++ web/src/app/user/add-property/add-property.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-add-property',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-property.page.scss'],
 })
 export class AddPropertyPage implements OnInit {
+  selectedOption: any;
+  showModal: boolean = false;
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private modalController: ModalController) { }
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
   }
 
+  openModal() {
+    if (this.selectedOption) {
+      this.showModal = true;
+    } else {
+      alert('Please select an option before opening the modal.');
+    }
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+
+  onSubmit() {
+    // Handle form submission here
+    console.log('Form submitted');
+    this.closeModal();
+  }
 }
+
+
+
+
