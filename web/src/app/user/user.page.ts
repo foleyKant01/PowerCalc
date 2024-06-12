97fa21dc465a3b0f41f12bad96940a9fb1948027
francis <francis.amani@tornixtech.com>
2024-06-12 12:22:01 +0000

    ajout du menu degache

--- web/src/app/user/user.page.ts
+++ web/src/app/user/user.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.page.html',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserPage implements OnInit {
 
+    //@ts-ignore
+    userInfo:any = JSON.parse(sessionStorage.getItem('infoLogin'))
+    is_user_logged_in = !!sessionStorage.getItem('infoLogin');
+
+
+  public appPages = [
+    { title: 'Home', url: '/user/home', icon: 'home' },
+    { title: 'History', url: '/user/history', icon: 'compass' },
+    { title: 'Notifications', url: '/user/notifications', icon: 'notifications' },
+    // { title: 'Profile', url: '/user/profile', icon: 'person' },
+    // { title: 'Favorites', url: '/user/favorites', icon: 'heart' },
+    // { title: 'My Tickets', url: '/user/tickets', icon: 'ticket' },
+
+  ];
+  public labels = ['Change secret code'];
+  
   constructor() { }
 
-  ngOnInit() {
+  logout(){
+    $.removeCookie('isLoggedIn', { path: '/' });
+    sessionStorage.removeItem('infoLogin')
+    window.location.href = '/'
   }
+
+  ngOnInit() {
+    // console.log(this.userInfo);
+    // throw new Error("Test Thrown Error");
+  }
+
 
 }
